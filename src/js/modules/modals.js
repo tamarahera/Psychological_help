14e9fbdd8e95d61c0dd3b9e1cd7b4dfd2c9d15b9
tamara_hera <tamara.herashchenko@gmail.com>
2023-07-11 12:05:08 +0200

    optimize modals

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,40 +1,52 @@
 const modals = () => {
+
+    function openModal(modalSelector, scrollNum) {
+        modalSelector.classList.add('modal--active');
+        document.body.style.overflow = 'hidden'; //прибирає скролл
+        if (window.screen.width >= 992) { //скролл стрибає тільки на комп'ютерах
+            document.body.style.marginRight = `${scrollNum}px`
+        }
+    }
+
+    function closeModal(modalSelector) {
+        modalSelector.classList.remove('modal--active');
+        document.body.style.overflow = '';
+        if (window.screen.width >= 992) {
+            document.body.style.marginRight = '';
+        }
+    }
+
     function initModal(triggerSelector, modalSelector, closeSelector) {
 
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
             close = document.querySelector(closeSelector),
             scroll = calcScroll();
+
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
                 if (e.target) {
                     e.preventDefault();
                 }
 
-                modal.classList.add('modal--active');
-                document.body.style.overflow = 'hidden'; //прибирає скролл
-                if (window.screen.width >= 992) { //скролл стрибає тільки на комп'ютерах
-                    document.body.style.marginRight = `${scroll}px`
-                }
+                openModal(modal, scroll);
             });
         })
         close.addEventListener('click', () => {
-            modal.classList.remove('modal--active');
-            document.body.style.overflow = '';
-            if (window.screen.width >= 992) {
-                document.body.style.marginRight = '';
-            }
+            closeModal(modal);
         });
 
         modal.addEventListener('click', (e) => {
             if (e.target === modal) {
-                modal.classList.remove('modal--active');
-                document.body.style.overflow = '';
-                if (window.screen.width >= 992) {
-                    document.body.style.marginRight = '';
-                }
+                closeModal(modal);
             }
-        })
+        });
+
+        window.addEventListener('keydown', (e) => {
+            if (modal.classList.contains('modal--active') && e.key === 'Escape') {
+                closeModal(modal);
+            }
+        });
     }
 
     function calcScroll() {
